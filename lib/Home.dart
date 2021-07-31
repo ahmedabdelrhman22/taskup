1979aaaad1b48c92e4a322cebb91027368b63add
ahmed abdelrhman <ahmedabdelrhman81@stud.fci-cu.edu.eg>
2021-07-31 23:40:38 +0200

    Second commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -38,6 +38,7 @@ class _HomeState extends State<Home> {
         "Posts"
         );
     postref.once(
+
     ).then(
             (DataSnapshot snap) {
           // ignore: non_constant_identifier_names
